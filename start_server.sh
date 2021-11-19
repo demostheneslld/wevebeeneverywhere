602cfef8d114d4a6ab68f31a8c66c3379556c9f8
Nathan Clement <nathan.clement.j@gmail.com>
2021-11-19 08:06:11 -0800

    Static fixes

--- start_server.sh
+++ start_server.sh
@@ -8,6 +8,6 @@ cd ../
 echo 'Activating Virtual Environment'
 source ./_env/bin/activate
 echo 'Collecting Static Files'
-python3 manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput -i ckeditor
 echo 'Running Application'
 python3 manage.py runserver 127.0.0.1:8000
--- wbe/settings.py
+++ wbe/settings.py
@@ -130,11 +130,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, '_staticfiles')
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'staticfiles'),
-# ]
-
 AZURE_ACCOUNT_NAME = 'wevebeeneverywhere'
 AZURE_ACCOUNT_KEY = environment.AZURE_STORAGE_CONNECTION_STRING
 AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
